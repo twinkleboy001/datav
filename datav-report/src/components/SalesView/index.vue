b15bbd1227cfe908568e1e3b26214d5f60ab2aae
zhouhao <18190816904@163.com>
2023-08-01 21:29:22 +0800

    feat: 销售趋势图表

--- datav-report/src/components/SalesView/index.vue
+++ datav-report/src/components/SalesView/index.vue
@@ -92,7 +92,61 @@ export default {
                 
             ]
           },
-          chartOption: {},
+          chartOptions: {
+            title: {
+              text: '年度销售额',
+              textStyle: {
+                fontSize: 12,
+                color: '#666'
+              },
+              left: 25,
+              top: 20
+            },
+            xAxis: {
+              type: 'category',
+              data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+              axisTick: {
+                alignWithLabel: true,
+                lineStyle: {
+                  color: '#999'
+                }
+              },
+              axisLine: {
+                lineStyle: {
+                  color: '#999'
+                }
+              },
+              axisLabel: {
+                color: '#333'
+              }
+            },
+            yAxis: {
+              axisLine: {
+                show: false
+              },
+              axisTick: {
+                show: false
+              },
+              splitLine: {
+                lineStyle: {
+                  type: 'dotted',
+                  color: '#eee'
+                }
+              }
+            },
+            series: [{
+              type: 'bar',
+              barWidth: '35%',
+              data: [200, 250, 300, 350, 300, 250, 200, 250, 300, 350, 300, 250]
+            }],
+            color: ['#3398DB'],
+            grid: {
+              top: 70,
+              left: 60,
+              right: 60,
+              bottom: 50
+            }
+          },
           rankData: [
             {
               no: 1,
